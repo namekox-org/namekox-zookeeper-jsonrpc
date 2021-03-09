cda778521d497be8a0a4ccb42d676b46293b75fa
李满满 <limanman@corp.netease.com>
2021-03-09 13:36:51 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-zookeeper-jsonrpc',
-    version='0.0.2',
+    version='0.0.3',
     description='namekox zookeeper jsonrpc',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='forcemain',
     url='https://forcemain.github.io/',
     packages=find_packages(exclude=['test', 'test.*']),
-    install_requires=["namekox-zookeeper>=0.0.1", "namekox-config>=0.0.1", "namekox-context>=0.0.1", "namekox-jsonrpc>=0.0.1"],
+    install_requires=["namekox-zookeeper", "namekox-config", "namekox-context", "namekox-jsonrpc"],
     zip_safe=True,
     license='Apache License, Version 2.0',
     classifiers=[
